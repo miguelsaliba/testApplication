55c3cd5c6172a8e860152d9dedefc2d5463382f8
miguelsaliba <36637899+miguelsaliba@users.noreply.github.com>
2019-11-25 19:14:52 +0300

    added different operators

--- app/src/main/java/com/example/testapplication/DisplayMessageActivity.java
+++ app/src/main/java/com/example/testapplication/DisplayMessageActivity.java
@@ -19,23 +19,34 @@ public class DisplayMessageActivity extends AppCompatActivity {
         Intent intent = getIntent();
         String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
 
-        simplify(message);
+        assert message != null;
+        message = simplify(message);
 
         // Capture the layout's TextView and set the string as its text
         TextView textView = findViewById(R.id.textView);
         textView.setText(message);
     }
 
-    private void simplify(String message) {
+    private String simplify(String message) {
         // Separate + sign
 
-        for (int i = 0; i < message.length(); i++) {
-            if (message.charAt(i) == '+'){
+        if (message.indexOf('+') >= 0) {
+            String[] parts = message.split("\\+");
+            message = String.valueOf(Integer.parseInt(parts[0]) + Integer.parseInt(parts[1]));
 
-            }
+        } else if (message.indexOf('-') >= 0) {
+            String[] parts = message.split("-");
+            message = String.valueOf(Integer.parseInt(parts[0]) - Integer.parseInt(parts[1]));
+
+        } else if (message.indexOf('/') >= 0) {
+            String[] parts = message.split("/");
+            message = String.valueOf(Integer.parseInt(parts[0]) / Integer.parseInt(parts[1]));
+
+        } else if (message.indexOf('*') >= 0) {
+            String[] parts = message.split("\\*");
+            message = String.valueOf(Integer.parseInt(parts[0]) * Integer.parseInt(parts[1]));
+
         }
-        assert message != null;
-        String[] parts = message.split("\\+");
-        message = String.valueOf(Integer.parseInt(parts[0]) + Integer.parseInt(parts[1]));
+        return message;
     }
 }
