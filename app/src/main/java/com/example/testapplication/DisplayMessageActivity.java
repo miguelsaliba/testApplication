e680c5b6268a7cf95f0ea5b8a135d7c11509aaee
miguelsaliba <36637899+miguelsaliba@users.noreply.github.com>
2019-11-26 23:43:24 +0300

    takes multiple operations in the same line but doesn't follow order of operations

--- app/src/main/java/com/example/testapplication/DisplayMessageActivity.java
+++ app/src/main/java/com/example/testapplication/DisplayMessageActivity.java
@@ -1,12 +1,10 @@
 package com.example.testapplication;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
 
-import java.util.regex.Pattern;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class DisplayMessageActivity extends AppCompatActivity {
 
@@ -28,8 +26,46 @@ public class DisplayMessageActivity extends AppCompatActivity {
     }
 
     private String simplify(String message) {
-        // Separate + sign
+        // Separate operation signs
 
+        int lastMult = message.lastIndexOf("*");
+        int lastDiv = message.lastIndexOf("/");
+        int lastSub = message.lastIndexOf("-");
+        int lastAdd = message.lastIndexOf("+");
+
+        double num;
+
+        if (lastMult > lastDiv && lastMult > lastSub && lastMult > lastAdd){
+            num = Double.parseDouble(message.substring(lastMult+1));
+            message = message.substring(0,lastMult);
+
+            return String.valueOf(Double.parseDouble(simplify(message)) * num);
+
+        }
+        if (lastDiv > lastSub && lastDiv > lastAdd){
+            num = Double.parseDouble(message.substring(lastDiv+1));
+            message = message.substring(0,lastDiv);
+
+            return String.valueOf(Double.parseDouble(simplify(message)) / num);
+
+        }
+        if (lastSub > lastAdd){
+            num = Double.parseDouble(message.substring(lastSub+1));
+            message = message.substring(0,lastSub);
+
+            return String.valueOf(Double.parseDouble(simplify(message)) - num);
+
+        } else if (lastAdd > lastSub){
+            num = Double.parseDouble(message.substring(lastAdd+1));
+            message = message.substring(0,lastAdd);
+
+            return String.valueOf(Double.parseDouble(simplify(message)) + num);
+
+        } else {
+            return message;
+        }
+
+        /*
         if (message.indexOf('+') >= 0) {
             String[] parts = message.split("\\+");
             message = String.valueOf(Integer.parseInt(parts[0]) + Integer.parseInt(parts[1]));
@@ -47,6 +83,7 @@ public class DisplayMessageActivity extends AppCompatActivity {
             message = String.valueOf(Integer.parseInt(parts[0]) * Integer.parseInt(parts[1]));
 
         }
-        return message;
+
+         */
     }
 }
